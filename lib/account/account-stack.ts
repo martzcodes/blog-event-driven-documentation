f289dd35b86e434704f0177faf93af2eb7bbf949
Matt Martz <matt.martz@gmail.com>
2022-10-22 22:41:59 -0400

    cross-account forwarding

--- lib/account/account-stack.ts
+++ lib/account/account-stack.ts
@@ -1,15 +1,29 @@
 import { Duration, RemovalPolicy, Stack, StackProps } from "aws-cdk-lib";
-import { EventBus } from "aws-cdk-lib/aws-events";
+import { EventBus, Rule } from "aws-cdk-lib/aws-events";
 import { AccountPrincipal } from "aws-cdk-lib/aws-iam";
 import { BlockPublicAccess, Bucket, ObjectOwnership } from "aws-cdk-lib/aws-s3";
 import { Construct } from "constructs";
+import { Source } from "../shared";
 import { CFListener } from "./cloudformation/cf-listener-construct";
 
+export interface AccountStackProps extends StackProps {
+  watcherAccount?: string;
+}
+
 export class AccountStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+  constructor(scope: Construct, id: string, props: AccountStackProps) {
     super(scope, id, props);
 
     const bus = EventBus.fromEventBusName(this, `bus`, "default");
+    const { watcherAccount } = props;
+    if (watcherAccount && watcherAccount !== Stack.of(this).account) {
+      new Rule(this, `WatcherFwd`, {
+        eventPattern: {
+          source: [Source],
+        }
+      });
+    }
+
     const specBucket = new Bucket(this, `AccountSpecBucket`, {
       removalPolicy: RemovalPolicy.DESTROY,
       blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
